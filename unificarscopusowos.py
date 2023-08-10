fee1c63cd1f0f1f63433912457e115dffd97e974
aavilesv <aavilesv@unemi.edu.ec>
2023-08-10 11:18:46 -0500

    unificarscopuswos

--- unificarscopusowos.py
+++ unificarscopusowos.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 
 # Cargar los archivos
@@ -9,5 +11,33 @@ columnascopus = ['Authors', 'Title', 'Source title', 'Year', 'Cited by', 'Affili
 columnaswebofscience = ['Authors', 'Article Title', 'Source Title', 'Publication Year', 'Cited Reference Count', 'Affiliations', 'Abstract', 'Author Keywords', 'Keywords Plus','Document Type','Addresses','DOI','DOI Link']
 df_scopus = df_scopus[columnascopus]
 df_wos = df_wos[columnaswebofscience]
-print(df_wos)
-print(df_scopus)
+# Convertir las columnas relevantes a mayúsculas
+df_scopus['Title'] = df_scopus['Title'].str.upper()
+df_scopus['Source title'] = df_scopus['Source title'].str.upper()
+df_wos['Article Title'] = df_wos['Article Title'].str.upper()
+df_wos['Source Title'] = df_wos['Source Title'].str.upper()
+
+
+# Renombrar las columnas en df_wos para que coincidan con las de df_scopus
+df_wos_renombrado = df_wos.rename(columns={
+    'Article Title': 'Title',
+    'Source Title': 'Source title',
+    'Publication Year': 'Year',
+    'Cited Reference Count': 'Cited by',
+    'Keywords Plus': 'Index Keywords',
+    'Addresses': 'Authors with affiliations',
+    'DOI Link': 'Link'
+})
+
+# Unir los DataFrames
+df_unido = pd.concat([df_scopus, df_wos_renombrado], ignore_index=True)
+
+## Encontrar los registros repetidos, manteniendo solo la primera aparición de cada valor duplicado
+repetidos_mask = df_unido.duplicated(subset=['Title'], keep='first')# subset debes poner las variables que deseas saber si se repiten
+df_repetidos = df_unido[repetidos_mask]
+# Encontrar los registros únicos, incluyendo la primera aparición de cada valor duplicado
+df_unicos = df_unido[~df_unido.duplicated(subset=['Title'], keep=False)]
+
+# Imprimir la cantidad de registros en cada DataFrame
+print("Cantidad de registros repetidos (sin incluir la primera aparición):", len(df_repetidos))
+print("Cantidad de registros únicos (incluyendo la primera aparición de valores duplicados):", len(df_unicos))
