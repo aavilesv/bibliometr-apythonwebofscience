7b2f9899a39b31b65ed9cf76ee15a72e602aeeda
aavilesv <aavilesv@unemi.edu.ec>
2023-09-05 17:08:46 -0500

    busquedascopus

--- busquedapalabrascopus.py
+++ busquedapalabrascopus.py
@@ -1,24 +1,43 @@
+
 import pandas as pd
 
+# Constantes
+RUTA_ARCHIVO_SCOPUS = 'C:\\Investigación\\Trabajo_2023\\Msc. Isabel Leal\\Búsqueda 1\\bibliometriascopusnew1csv.xlsx'
+RUTA_ARCHIVO_FILTRADO = 'df_scopus_filtrado.xlsx'
+#COLUMNAS_BUSQUEDA = ['abstract', 'affiliations', 'author_keywords', 'keywords', 'title']
+COLUMNAS_BUSQUEDA = ['affiliations', 'author_keywords', 'keywords', 'title']
 # Cargar el archivo de Scopus
-df_scopus = pd.read_excel('C:\\Investigación\\Trabajo_2023\\Codificaciones\\archivo_scopus.xlsx')
+df_scopus = pd.read_excel(RUTA_ARCHIVO_SCOPUS)
 
-# Ajusta estos valores según las categorías y palabras clave que quieras filtrar en Scopus
-valores_scopus = ['Computer Science', 'Information Science', 'Technology', 'computacion de ciencias']
-palabras_scopus = ['web application', 'website', 'evaluation', 'evaluate']
+# Filtrar las columnas que realmente existen en el DataFrame
+columnas_existentes = [col for col in COLUMNAS_BUSQUEDA if col in df_scopus.columns]
 
-regex_scopus = r'^(.*\s)?(' + '|'.join(valores_scopus) + r')(\s.*)?$'
-df_filtradoarea_scopus = df_scopus[df_scopus['categories_column_name'].str.split(';').apply(lambda x: any(pd.Series(x).str.contains(regex_scopus, case=False, na=False)))]
+# Palabras clave para buscar en Scopus
+palabras_scopus = [
+    'bibliometrics', 'Bibliometric', 'Scopus', 'Visual analysis', 'Systematic literature review',
+    'bibliometrix', 'biblioshby', 'Citation analysis', 'Content analysis', 'Publication trend',
+    'Scientometrics', 'Informetrics', 'Heuristic evaluation', 'Heuristic analysis', 'Bibliometric mapping',
+    'Co-citation analysis', 'Authorship pattern', 'heuristic method', 'heuristic approach', 'heuristic technique'
+]
 
-def contains_words_scopus(text): 
-    text = '' if pd.isnull(text) else text
-    return pd.Series(text.split(';')).str.contains('|'.join([rf'\b{word}\b' for word in palabras_scopus]), case=False, na=False).any()
 
-def contains_words_no_split_scopus(text):
-    text = '' if pd.isnull(text) else text
-    return pd.Series(text).str.contains('|'.join([rf'\b{word}\b' for word in palabras_scopus]), case=False, na=False).any()
+# Función de búsqueda
+def search_for_word(word):
+    def contains_word(text): 
+        text = '' if pd.isnull(text) else text
+        return word in text
+    mask = df_scopus[columnas_existentes].applymap(contains_word).any(axis=1)
+    return df_scopus[mask]
 
-df_filtrado_scopus = df_filtradoarea_scopus[df_filtradoarea_scopus[['keywords_column_name', 'other_keywords_column_name']].applymap(contains_words_scopus).any(axis=1) | df_filtradoarea_scopus[['title_column_name', 'abstract_column_name']].applymap(contains_words_no_split_scopus).any(axis=1)]
+# Buscar cada palabra y concatenar los resultados
+dfs_filtrados = [search_for_word(word) for word in palabras_scopus]
+df_filtrado_scopus = pd.concat(dfs_filtrados).drop_duplicates()
+
+# Extraer el número de citas y asignarlo a una nueva columna
+df_filtrado_scopus['Cited_by'] = df_filtrado_scopus['note'].str.extract(r'Cited by: (\d+)').astype(float)
+
+# Extraer la información de acceso abierto y asignarla a una nueva columna
+df_filtrado_scopus['open_access'] = df_filtrado_scopus['note'].str.extract(r'(All Open Access, Gold Open Access, Green Open Access)')
 
 # Guardar el DataFrame filtrado
-df_filtrado_scopus.to_excel('df_scopus_filtrado.xlsx', index=False)
+df_filtrado_scopus.to_excel(RUTA_ARCHIVO_FILTRADO, index=False)
