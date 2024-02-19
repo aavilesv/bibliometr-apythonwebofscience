255fad6b4b857453197a834e32eac11bcec0e319
aavilesv <aavilesv@unemi.edu.ec>
2024-02-19 17:13:50 -0500

    Citaciones_year

--- Bibliometrix_citaciones_medias_anuales.py
+++ Bibliometrix_citaciones_medias_anuales.py
@@ -13,18 +13,18 @@ data = {
 df = pd.DataFrame(data)
 # Define the offset for positioning the data labels for MeanTCperArt
 mean_tc_per_art_offsets = [(-5, 10), (-5, 10), (-1, 10), (12, 8), (-15, 10),
-                           (-15, 10), (-15, -15), (-15, -15), (-15, -15), (-15, -15), (-15, -15)]
+                           (-20, 0), (-15, -15), (-15, -15), (-15, -15), (-15, -15), (-15, -15)]
 
 # Define the offset for positioning the data labels for MeanTCperYear
-mean_tc_per_year_offsets = [(-15, -15), (-15, -15), (-15, -15), (-15, 10), (-15, -15),
-                            (-15, -15), (-15, 10), (-15, 10), (-15, 10), (-15, 10), (-15, 10)]
+mean_tc_per_year_offsets = [(-5, 7), (-8, 15), (0, 8), (8, 10), (15, -15),
+                            (20, -15), (15, 10), (4, 12), (0, 10), (15, 10), (5, 10)]
 
 
 # Create figure and axis objects with subplots()
 fig, ax = plt.subplots(figsize=(15, 7))
 
 # Bar plot for the number of articles
-bars = ax.bar(df["Year"], df["N"], color="skyblue", width=0.65, label="Número de Artículos")
+bars = ax.bar(df["Year"], df["N"], color="skyblue", width=0.65, label="Number of Items")
 
 # Adding data labels to the bars
 for bar in bars:
@@ -33,39 +33,40 @@ for bar in bars:
 
 # Line plot for MeanTCperArt
 ax2 = ax.twinx()
-line1, = ax2.plot(df["Year"], df["MeanTCperArt"], color="red", marker="o", label="MeanTCperArt", linestyle='--')
+line1, = ax2.plot(df["Year"], df["MeanTCperArt"], color="darkslategrey", marker="o", label="MeanTCperArt", linestyle='--')
 
-# Adding data labels to the line plot for MeanTCperArt
+# Añadir etiquetas de datos al gráfico de líneas para MeanTCperArt
 for i, (txt, (dx, dy)) in enumerate(zip(df["MeanTCperArt"], mean_tc_per_art_offsets)):
     ax2.annotate(f"{txt:.2f}", (df["Year"][i], df["MeanTCperArt"][i]), textcoords="offset points", 
                  xytext=(dx, dy), ha='center', fontsize=10)
-# Line plot for MeanTCperYear
+#Gráfico lineal de la media anual de TCT
 ax3 = ax.twinx()
 line2, = ax3.plot(df["Year"], df["MeanTCperYear"], color="green", marker="s", label="MeanTCperYear")
 ax3.spines["right"].set_position(("axes", 1.15))  # Offset the right spine of ax3
 
-# Adding data labels to the line plot for MeanTCperYear
+# Añadir etiquetas de datos al gráfico de líneas para MeanTCperYear
 
 for i, (txt, (dx, dy)) in enumerate(zip(df["MeanTCperYear"], mean_tc_per_year_offsets)):
     ax3.annotate(f"{txt:.2f}", (df["Year"][i], df["MeanTCperYear"][i]), textcoords="offset points", 
                  xytext=(dx, dy), ha='center', fontsize=10)
-# Set the y axis labels
-ax.set_ylabel('Número de Artículos', fontsize=14)
-ax2.set_ylabel('Promedio de Citas por Artículo', fontsize=14)
-ax3.set_ylabel('Promedio de Citas por Año', fontsize=14)
+# Establecer las etiquetas del eje y
+ax.set_ylabel('Number of Items', fontsize=14, color='black')
+ax2.set_ylabel('Average number of citations per article', fontsize=14, color='black')
+ax3.set_ylabel('Average Number of Citations per Year', fontsize=14, color='black')
 
-# Set the x axis label
-ax.set_xlabel('Año', fontsize=14)
+# Establecer la etiqueta del eje x
+ax.set_xlabel('Year', fontsize=14, color='black')
 
-# Set the title
-plt.title('Estudio Bibliométrico: Artículos y Citas por Año', fontsize=16)
 
-# Set the legend
+# Establecer el título
+plt.title('Bibliometric Study: Articles and Citations by Year', fontsize=16)
+
+# Establecer la leyenda
 ax.legend(loc='upper left')
 ax2.legend(loc='upper right')
 ax3.legend(loc='center right')
 
-# Adjust the layout to make room for the labels
+# Ajustar el diseño para hacer espacio para las etiquetas
 plt.tight_layout()
 
 # Show plot
