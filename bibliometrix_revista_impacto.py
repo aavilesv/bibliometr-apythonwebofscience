370270752b2d5a4e522a161b704fa92e167292f3
aavilesv <aavilesv@unemi.edu.ec>
2024-02-20 17:31:49 -0500

    Revista

--- bibliometrix_revista_impacto.py
+++ bibliometrix_revista_impacto.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np  # Añade la importación de numpy para generar colores únicos
 
 # Leer el archivo Excel
-ruta_archivo = 'C:\\Users\\AAVILESV\\Downloads\\Source_Impact.xlsx'
+ruta_archivo = 'G:\\Mi unidad\\2024\\Msc. Jorge Vinueza\\informaciondata\\wos_source_Impact.xlsx'
 df = pd.read_excel(ruta_archivo)
 
 # Asegurarse de que las columnas estén en el formato correcto
@@ -33,3 +33,47 @@ plt.tight_layout()
 plt.show()
 
 
+'''
+#ruta_archivo = 'C:\\Users\\AAVILESV\\Downloads\\Most_Relevant_Sources.xlsx'
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np  # Añade la importación de numpy para generar colores únicos
+
+# Leer el archivo Excel
+ruta_archivo = 'G:\\Mi unidad\\2024\\Msc. Jorge Vinueza\\informaciondata\\wos_source_Impact.xlsx'
+
+
+# Leer el archivo Excel
+df = pd.read_excel(ruta_archivo)
+offsets = [(-5, 10), (-5, 10), (-1, 10), (12, 8), (-15, 10),
+           (-20, 0), (-15, -15), (-15, -15), (-15, -15), (-15, -15), (-15, -15)]
+
+# Asegurarse de que las columnas estén en el formato correcto
+columnas_esperadas = ["Element", "h_index", "g_index", "m_index", "TC", "NP", "PY_start"]
+if not all(col in df.columns for col in columnas_esperadas):
+    raise ValueError("El archivo Excel no tiene las columnas esperadas.")
+
+# Crear gráfico de dispersión
+plt.figure(figsize=(12, 7))
+
+# Iterar sobre cada fila para plotear y anotar el punto
+for index, row in df.iterrows():
+    plt.scatter(row['h_index'], row['TC'], color='blue')  # Puedes personalizar el color si lo deseas
+    plt.text(row['h_index'], row['TC'], row['Element'], fontsize=9)
+
+# Calcular y dibujar la línea de tendencia
+z = np.polyfit(df['h_index'], df['TC'], 1)
+p = np.poly1d(z)
+plt.plot(df['h_index'], p(df['h_index']), "r--")
+
+# Títulos y etiquetas
+plt.title("Sources' Local Impact: h index vs Total Citations")
+plt.xlabel('h_index')
+plt.ylabel('Total Citations (TC)')
+
+# Rejilla, ajuste de diseño y mostrar el gráfico
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+'''
